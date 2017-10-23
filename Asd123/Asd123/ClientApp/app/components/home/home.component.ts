6c23d0ba9e6598f15a6e996156af0c25ee1f3a84
szabolcsmagyar8 <ubulon@gmail.com>
2017-10-23 15:52:59 +0200

    very simple file browser dialog implemented with an upload button, which only shows an alert

--- Asd123/Asd123/ClientApp/app/components/home/home.component.ts
+++ Asd123/Asd123/ClientApp/app/components/home/home.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'home',
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
+
+    constructor() { }
+
+    ngOnInit() {
+        alert("oninit! :)");
+    }
+
+    myFunc() {
+        alert("Upload successful");
+    }
+
+    //$(function() {
+
+    //    // We can attach the `fileselect` event to all file inputs on the page
+    //    $(document).on('change', ':file', function () {
+    //        var input = $(this),
+    //            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+    //            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    //        input.trigger('fileselect', [numFiles, label]);
+    //    });
+
+    //});
 }
