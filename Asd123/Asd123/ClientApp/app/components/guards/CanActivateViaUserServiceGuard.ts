f9b8ae97eaefedff5617b3ad8b28041be35f0a74
balzska <varesz11@gmail.com>
2017-11-12 19:23:10 +0100

    fixed logged in guard issue, where the functionality did not work well with async user info get

--- Asd123/Asd123/ClientApp/app/components/guards/CanActivateViaUserServiceGuard.ts
+++ Asd123/Asd123/ClientApp/app/components/guards/CanActivateViaUserServiceGuard.ts
@@ -1,14 +1,22 @@
 ﻿import { Injectable } from '@angular/core';
-import { CanActivate } from '@angular/router';
+import { CanActivate, Router } from '@angular/router';
 import { User } from '../userservice/user';
 import { UserService } from "../userservice/userservice.component";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class CanActivateViaUserLoggedInGuard implements CanActivate {
-    constructor(private userService: UserService) {
+    private user: User;
+    constructor(private userService: UserService, private router: Router) {
     }
 
     canActivate() {
-        return this.userService.getLoggedInUser() != null;
+        return this.userService.getLoggedInUser().map(
+            user => {
+                if (user == null)
+                    this.router.navigate(['/home']);
+                return user != null;
+            }
+        );
     }
 }
