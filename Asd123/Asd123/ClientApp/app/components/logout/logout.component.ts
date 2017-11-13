67575fa2761d2083293cd02f4454502141315354
kotex17 <kotipeter@gmail.com>
2017-11-13 14:15:13 +0100

    logout 1

--- Asd123/Asd123/ClientApp/app/components/logout/logout.component.ts
+++ Asd123/Asd123/ClientApp/app/components/logout/logout.component.ts
@@ -1,17 +1,39 @@
 ﻿import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable, Inject } from '@angular/core';
+import { Http, Jsonp, Response, Headers, RequestOptions } from '@angular/http';
+import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../userservice/userservice.component";
+import { User } from "../userservice/user";
 
 @Component({
     selector: 'logout',
     templateUrl: './logout.component.html'
 })
+
+@Injectable()
 export class LogoutComponent {
 
-    public LogoutComponent() {
+    private _http: Http;
+    private _baseUrl: string;
+    private userservice: UserService;
 
+    constructor( userservice: UserService) {
+
+        this.userservice = userservice;
+        /*http: Http, @Inject('BASE_URL') baseUrl: string*/
+       /* this._http = http;
+        this._baseUrl = baseUrl;*/
+        //   this._http.post(this._baseUrl + 'api/account/logout', null);
     }
 
-    public logoutUser(){}
+    logOutUser() {
+      return this.userservice.logOutUser;
+      
+       
+    }
+
+
+   
 }
 
 
