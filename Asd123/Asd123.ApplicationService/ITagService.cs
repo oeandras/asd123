15753790415fabd8bf5fe115e99ea573895ae140
Czirják Gergő <czgergo26@gmail.com>
2017-12-09 12:59:45 +0100

    tagservice interface

--- Asd123/Asd123.ApplicationService/ITagService.cs
+++ Asd123/Asd123.ApplicationService/ITagService.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Asd123.Domain;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Asd123.ApplicationService
 {
     public interface ITagService
     {
+        Task<Tag> GetByText(string text);
+        Task<Tag> GetById(Guid id);
+        Task<IReadOnlyCollection<Tag>> GetByPicture(Guid pictureId);
+        Task AddToPicture(IEnumerable<string> texts, IEnumerable<Guid> pictureIds);
+        Task RemoveFromPicture(IEnumerable<string> texts, IEnumerable<Guid> pictureIds);
+        Task<Tag> CreateTag(string text);
+        Task DeleteTag(Tag tag);
     }
 }
