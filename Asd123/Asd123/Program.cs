a6c9b400f546d00296bcfcf486bef9d569014ea5
Czirják Gergő <czgergo26@gmail.com>
2017-12-09 09:31:34 +0100

    added ssl cert for azure

--- Asd123/Asd123/Program.cs
+++ Asd123/Asd123/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Asd123
 {
@@ -29,12 +30,41 @@ namespace Asd123
                 {
                     options.Listen(IPAddress.Loopback, 44301, listenOptions =>
                     {
-                        listenOptions.UseHttps("sslcert/self-signed-cert.pfx", "alma");
+                        var cert = GetCert();
+                        listenOptions.UseHttps(cert);
                     });
                 })
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
+
+
+        public static X509Certificate2 GetCert()
+        {
+            X509Certificate2 cert = null;
+            using (X509Store certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser))
+            {
+                certStore.Open(OpenFlags.ReadOnly);
+                X509Certificate2Collection certCollection = certStore.Certificates.Find(
+                    X509FindType.FindByThumbprint,
+                    // Replace below with your cert's thumbprint
+                    "6d2ce765fef1fc4f672d20f686e727b8e97816d7",
+                    false);
+                // Get the first cert with the thumbprint
+                if (certCollection.Count > 0)
+                {
+                    cert = certCollection[0];
+                }
+            }
+
+            // Fallback to local file for development
+            if (cert == null)
+            {
+                cert = new X509Certificate2(Path.Combine(Directory.GetCurrentDirectory(), "example.pfx"), "exportpassword");
+            }
+
+            return cert;
+        }
     }
 }
